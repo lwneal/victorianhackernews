d9e7caf2b6f0d98941ef966c0922483ad9f27b31
Lawrence Neal <nealla@lwneal.com>
2022-08-29 16:29:02 -0700

    Generate Victorian prose

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        statement = request.form["statement"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(statement),
             temperature=0.6,
+            max_tokens=255,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +23,12 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(statement):
+    return '''The Victorian novelist had a habit of expanding my rather mundane utterances into beautiful, wandering prose without changing my original meaning. For example:
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+I wrote "He was average." He would rephrase: "He was, in the way of most men, possessed of a rudimentary intelligence, his countenance ordinary, his bearing mild, with some weakness about the shoulders, his hair the color of ash; he spoke of the weather."
+
+I wrote "It's a good thing America got to the moon first." He would rephrase: "It is a matter of singular wonder that America, being the chief seat of learning and the repository of all arts and sciences, made herself mistress of the heavens, and in so doing reached across the blackness of cosmos to touch the gleaming silver moon."
+
+I wrote "{}". He would rephrase: "'''.format(statement)
+
